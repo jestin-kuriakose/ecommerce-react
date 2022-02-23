00c37a248aa7ddc044abc9d2206bab34dfab0233
ilir pllana <ilirpllana95@gmail.com>
2022-02-22 21:04:48 -0500

    added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,13 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
 import styled from "styled-components"
+import sliderItems from "../data"
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `
 const Arrow = styled.div`
   width: 50px;
@@ -16,15 +18,85 @@ const Arrow = styled.div`
   align-items: center;
   justify-content: center;
   position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  left: ${props => props.direction === "left" && "10px"};
+  right: ${props => props.direction === "right" && "10px"};
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transform: translateX(-200vw);
+`
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items:center;
+  background-color: #${props=> props.bg};
+`
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`
+
+const Image = styled.img`
+  height: 80%;`
+
+const InfoContainer = styled.div`
+  flex: 1;
+`
+
+const Title = styled.h1`
+  font-size: 70px;
+`
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `
 
 const Slider = () => {
+
+  const handleClick = (direction) => {
+    
+  }
+
   return (
     <Container>
-      <Arrow>
+      <Arrow direction="left" onClick={()=> handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Arrow>
+      <Wrapper>
+
+        <Slide bg="f5fafd">
+          <ImgContainer>
+            <Image src="https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png"/>
+          </ImgContainer>
+          <InfoContainer>
+            <Title>SUMMER SALE</Title>
+            <Desc>DON'T COMPROMISE ON STYLE !</Desc>
+            <Button>SHOP NOW</Button>
+          </InfoContainer>
+        </Slide>
+        
+        
+
+      </Wrapper>
+      <Arrow direction="right" onClick={()=> handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const slider = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE !",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE !",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE !",
+        bg: "fbf0f4"
+    }
+]
