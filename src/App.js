f1bdb0e9c2411a7eeb4da732c7e388f0387dcdfe
ilir pllana <ilirpllana95@gmail.com>
2022-03-14 15:47:47 -0400

    added cart page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-    <Register />
+    <Cart />
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url("https://i.ibb.co/307s8L1/pexels-disha-sheta-3306174.jpg") center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-size: cover;
+`
+const Wrapper = styled.div`
+    width: 25%;
+    padding: 20px;
+    background-color: white;
+`
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0px;
+    padding: 10px;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 10px;
+`
+const Link = styled.a`
+    margin: 5px 0px;
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
+`
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Container>
+        <Wrapper>
+            <Title>SIGN IN</Title>
+            <Form>
+                <Input placeholder="username"/>
+                <Input placeholder="Password"/>
+                <Button>LOGIN</Button>
+                <Link>Forgot password?</Link>
+                <Link>Create a new account</Link>
+            </Form>
+        </Wrapper>
+    </Container>
   )
 }
 
