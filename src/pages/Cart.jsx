bd3191413f4b9c9374a3deedc168119c66a49158
ilir pllana <ilirpllana95@gmail.com>
2022-03-15 20:56:01 -0400

    finished with cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,6 +2,8 @@ import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import styled from 'styled-components'
+import { Add, Remove } from '@material-ui/icons'
+
 
 const Container = styled.div``
 const Wrapper = styled.div`
@@ -42,16 +44,21 @@ const Info = styled.div`
     flex: 3;
 `
 const Product = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
 `
 const ProductDetail = styled.div`
-    
+    flex: 2;
+    display: flex;
 `
 const Image = styled.img`
-    
+    width: 200px;
 `
 const Details = styled.div`
-    
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
 `
 const ProductName = styled.span`
     
@@ -60,14 +67,40 @@ const ProductId = styled.span`
     
 `
 const ProductColor = styled.div`
-    
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color}
 `
 const ProductSize = styled.span`
     
 `
 const PriceDetail = styled.span`
-    
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 `
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-item: center;
+    margin-bottom: 20px;
+`
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+`
+
 const Summary = styled.div`
     flex: 1;
 `
@@ -91,19 +124,56 @@ const Cart = () => {
                 <Info>
                     <Product>
                         <ProductDetail>
-                            <Image/>
+                            <Image src="https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png"/>
                             <Details>
                                 <ProductName><b>Product:</b>Apple iPhone 8 Plus 64gb Space Gray</ProductName>
                                 <ProductId><b>Product ID:</b>CPO-IP8PBK-64</ProductId>
-                                <ProductColor/>
+                                <ProductColor color="black"/>
                                 <ProductSize><b>Size:</b>64gb</ProductSize>
 
                             </Details>
                         </ProductDetail>
-                        <PriceDetail>Price</PriceDetail>
+                        <PriceDetail>
+                            <ProductAmountContainer>
+                                <Add/>
+                                <ProductAmount>2</ProductAmount>
+                                <Remove/>
+                            </ProductAmountContainer>
+                            <ProductPrice>$30</ProductPrice>
+                        </PriceDetail>
                     </Product>
+
+                    <Hr/>
+
+                    <Product>
+                        <ProductDetail>
+                            <Image src="https://i.ibb.co/Ybfw1WM/evie-s-vz3-IQy0-LOa-A-unsplash-removebg-preview.png"/>
+                            <Details>
+                                <ProductName><b>Product:</b>Apple iPhone 8 Plus 64gb Space Gray</ProductName>
+                                <ProductId><b>Product ID:</b>CPO-IP8PBK-64</ProductId>
+                                <ProductColor color="black"/>
+                                <ProductSize><b>Size:</b>64gb</ProductSize>
+
+                            </Details>
+                        </ProductDetail>
+                        <PriceDetail>
+                            <ProductAmountContainer>
+                                <Add/>
+                                <ProductAmount>2</ProductAmount>
+                                <Remove/>
+                            </ProductAmountContainer>
+                            <ProductPrice>$30</ProductPrice>
+                        </PriceDetail>
+                    </Product>
+
                 </Info>
-                <Summary>Summary</Summary>
+                <Summary>
+                    <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+                    <SummaryItem>
+                        <SummaryItemText>Subtotal</SummaryItemText>
+                        <SummaryItemPrice>$ 80</SummaryItemPrice>
+                    </SummaryItem>
+                </Summary>
             </Bottom>
         </Wrapper>
         <Footer/>
