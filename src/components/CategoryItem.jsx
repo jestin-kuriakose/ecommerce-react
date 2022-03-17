ce9bf25962203162f55b889ef374994f40fcfbd8
ilir pllana <ilirpllana95@gmail.com>
2022-03-17 16:56:51 -0400

    added media queries

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import {mobile} from "../responsive"
 
 const Container = styled.div`
     flex: 1;
@@ -9,7 +10,8 @@ const Container = styled.div`
 const Image = styled.img`
     width: 100%;
     height: 100%;
-    object-fir: cover;
+    object-fit: cover;
+    ${mobile({ height: "20vh" })};
 `
 const Info = styled.div`
     position: absolute;
